44d66088613b70dd44e4db586432bd3b1cd1f9bc
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-07-28 19:31:22 +0300

    Add User model with password hashing
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dateJoined: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Hash the password before saving the user
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
