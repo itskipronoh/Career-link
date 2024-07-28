57a6a266208b7758aab950a248686ed290c7eb3b
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-07-28 19:37:50 +0300

    Update MongoDB URI and add user authentication routes and functionality
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const jobRoutes = require('./routes/jobRoutes');
+const userRoutes = require('./routes/UserRoutes');
 
 dotenv.config();
 
@@ -15,11 +16,8 @@ app.use(express.json());
 
 // Routes
 app.use('/api', jobRoutes);
+app.use('/api', userRoutes);
 
-<<<<<<< HEAD
 const PORT = process.env.PORT || 5000;
-=======
-const PORT = process.env.PORT || 3000;
->>>>>>> origin/main
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
