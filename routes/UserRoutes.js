671dce0f45c0e6476cdd522108598437e29ba53c
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-07-28 19:51:16 +0300

    refactor
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/UserController');
-const auth = require('../middleware/auth'); // Middleware for protecting routes
+// const auth = require('../middleware/auth'); // Middleware for protecting routes
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.get('/profile', auth, userController.getUserProfile);
+router.get('/profile',userController.getUserProfile);
 
 module.exports = router;
